c971cd65d703d25f2d0a9245d99584889bd36ac6
jiyinyiyong <jiyinyiyong@gmail.com>
2012-09-10 21:20:33 +0800

    clear

--- watch.sh
+++ watch.sh
@@ -1,8 +1,5 @@
 
-echo 'should have installed:'
-echo 'jade, stylus, coffee-script , doodle, node-dev'
-echo "..."
-echo "..."
+echo '------> start watching'
 
 jade -O page/ -w src/index.jade &
 stylus -o page/ -w src/page.styl &
@@ -10,3 +7,14 @@ coffee -o page/ -wb src/handle.coffee &
 coffee -o page/ -wb src/action.coffee &
 doodle page/  server.coffee &
 node-dev server.coffee &
+
+read
+
+pkill -f 'jade -O page/ -w src/index.jade'
+pkill -f 'stylus -o page/ -w src/page.styl'
+pkill -f 'coffee -o page/ -wb src/handle.coffee'
+pkill -f 'coffee -o page/ -wb src/action.coffee'
+pkill -f 'doodle page/  server.coffee'
+pkill -f 'node-dev server.coffee'
+
+echo '------> stop watching'
